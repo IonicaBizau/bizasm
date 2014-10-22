3a971bf614dc1d5849ab5048741876761a24e704
Ionică Bizău <bizauionica@yahoo.com>
2014-10-22 15:47:45 +0300

    Added label in main window

--- BizMachineGUI/BizMachineGUI/MainWindow.cs
+++ BizMachineGUI/BizMachineGUI/MainWindow.cs
@@ -6,15 +6,22 @@ using Interpreter;
 public partial class MainWindow: Gtk.Window
 {	
 	static Gtk.Image img = new Gtk.Image();
+	static Gtk.Label registerLabel = new Gtk.Label ();
+
 	public void SetImageSrc(string src) {
 		//this.image1.Pixbuf = "";
 		img.Pixbuf = new Gdk.Pixbuf (src);
 	}
 
+	public void UpdateRegisterOutput(string output) {
+		registerLabel.Text = output;
+	}
+
 	public MainWindow (): base (Gtk.WindowType.Toplevel)
 	{
 		Build ();
-		img = image3;
+		img = image4;
+		registerLabel = label1;
 	}
 
 	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
